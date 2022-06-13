0e39df90e128865e783e5f7756bf69eb66569043
mauricio.coral <mauricio.coral>
2022-06-13 05:18:20 -0500

    Initial commit

---
+++ src/index.php
@@ -0,0 +1,31 @@
+<?php
+
+echo "POC Sproutloud - Containers <br><br>";
+
+// $mysqli = new mysqli("db", "root", "example", "company1");
+//
+// $sql = "truncate table users";
+// $result = $mysqli->query($sql);
+// $sql = "INSERT INTO users (name, fav_color) VALUES('Lil Sneazy', 'Yellow')";
+// $result = $mysqli->query($sql);
+// $sql = "INSERT INTO users (name, fav_color) VALUES('Nick Jonas', 'Brown')";
+// $result = $mysqli->query($sql);
+// $sql = "INSERT INTO users (name, fav_color) VALUES('Maroon 5', 'Maroon')";
+// $result = $mysqli->query($sql);
+// $sql = "INSERT INTO users (name, fav_color) VALUES('Tommy Baker', '043A2B')";
+// $result = $mysqli->query($sql);
+//
+//
+// $sql = 'SELECT * FROM users';
+//
+// if ($result = $mysqli->query($sql)) {
+//     while ($data = $result->fetch_object()) {
+//         $users[] = $data;
+//     }
+// }
+//
+// foreach ($users as $user) {
+//
+//     echo $user->name . " " . $user->fav_color;
+//     echo "<br>";
+// }
